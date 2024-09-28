574bb1bc9772973742c1d2e8254496d8558e744e
jhirarara <jairie200@gmail.com>
2024-09-29 06:45:57 +0800

    "123"

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const { join } = require('path');
 
 const app = express();
+
+// Use process.env.PORT for Railway deployment
 const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
@@ -12,120 +14,41 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-
-
-
-
-
-
-
-const on=()=>{
-
-
-    console.log('device on');
-}
-
-
-
-
-
 const io = socketIo(server, {
   cors: {
-    origin: "*", // Allow all origins
+    origin: "*", // Allow all origins for testing
     methods: ["GET", "POST"],
   }
 });
 
-
 // Handle WebSocket connections
 io.on('connection', (socket) => {
-    console.log('A user connected',socket.id);
+  console.log('A user connected', socket.id);
 
+  socket.on('joinRoom', (data) => {
+    socket.join(data.value);
+    io.to(data.value).emit('message', `User ${data.user} has joined the room`);
+  });
 
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', message.message);
+  });
 
+  socket.on('send', (data) => {
+    if (data === '') {
+      io.emit('message', "noroom"); // Broadcast message to all clients
+    } else {
+      console.log(data);
+      socket.to(data.room).emit('message', data.message);
+    }
+  });
 
-    socket.on('joinRoom', data=> {
-      socket.join(data.value);
-
- 
-  
-      io.to(data.value).emit('message', `User ${data.user} has joined the room`);
-    });
-
-
-
-
-
-
-
-
-    socket.on('message',(message)=>{
-console.log(message)
-
-  io.emit('message', message.message);
-
-
-
-
-    });
-
-
-
-
-socket.on('send',(data)=>{
-
-if(data===''){
-
-
-
-    
-        io.emit('message', "noroom"); // Broadcast message to all clients
- 
-
-
-}
-
-else{
-
-
-console.log(data)
-    socket.to(data.room).emit('message', data.message);
-
-  }
-
-
-});
-
-
-
-
-
-
-
-socket.on('room',(room)=>{
-
-console.log(room,socket.id)
-socket.join(room);
-
-
-
-
-
-
-
-console.log(room);
-
-})
-
-
-
-
-
-
-
-
-
-
+  socket.on('room', (room) => {
+    console.log(room, socket.id);
+    socket.join(room);
+    console.log(room);
+  });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
